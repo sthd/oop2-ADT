b22fa5c74b2d3e8234acd588d91c522d4a8120b5
sthd <sch@campus.technion.ac.il>
2022-05-29 12:49:02 +0300

    Added basic structure

--- oop2-ADT/src/homework2/BipartiteGraph.java
+++ oop2-ADT/src/homework2/BipartiteGraph.java
@@ -1,6 +1,11 @@
 package homework2;
 
-public class BipartiteGraph {
+import java.util.*;
+
+public class BipartiteGraph<T> {
+	
+	private final String name_;
+	private Map<T, List<T>> nodes_;
 	
 	// TODO repInvariant BipartiteGraph
 	// RepInvariant:
@@ -16,21 +21,24 @@ public class BipartiteGraph {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("work");
+		//System.out.println("work");
 
+		
+		
 	}
 	
 	
-	
     /**
      * @requires 
      * @modifies
      * @effects
      * 
      */
-    public BipartiteGraph() {
+    public BipartiteGraph(String name) {
     	//TODO
-    	
+    	this.name_= name;
+    	this.nodes_ = new HashMap<T, List<T>>();
+    	checkRep();
     }
 
     
@@ -40,11 +48,15 @@ public class BipartiteGraph {
      * @effects
      * 
      */
-    public boolean addBlackNode() {
+    public boolean addBlackNode(T blackNode) {
     	//TODO
+    	// make sure node isn't in graph (using unique label)
+    	
+    	
     	return true;
     }
     
+
     
     /**
      * @requires 
@@ -60,6 +72,27 @@ public class BipartiteGraph {
     }
     
     
+    /**
+     * @requires 
+     * @modifies
+     * @effects
+     * 
+     */
+    public boolean addEdge(T sourceNode, T destinationNode, T edgeLabel) {
+    	//TODO
+    	// check that nodes are not null
+    	// ?? check that the nodes are different??
+    	// ?? check that the nodes of different type (i.e. black or white) ??
+    	
+    	
+    	return true;
+    }
+    
+    @Override
+    public String toString() {
+    	String s = "What to override";
+    	return s;
+    }
     
     
     /**
