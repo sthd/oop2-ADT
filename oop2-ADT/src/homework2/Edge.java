5017d1c9d1c245df955ea42a0c0bbfc5cc7b51c4
baalon <baalon@LaptopBS>
2022-06-03 19:48:55 +0300

    Added New Nodes functions
    AddChildNode
    addParentNode

---
+++ oop2-ADT/src/homework2/Edge.java
@@ -0,0 +1,10 @@
+package homework2;
+
+import java.util.List;
+import java.util.Map;
+
+public class Edge<T> {
+    private T edgeLabel;
+    private Node src,dst;
+    
+}
--- oop2-ADT/src/homework2/Node.java
+++ oop2-ADT/src/homework2/Node.java
@@ -5,25 +5,42 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Node<T>{
-    private T nodeLabel;
-    private Map<T, Node<T>> parentNodes; // key is an edge, Value is a Node
-    private Map<T, Node<T>> childrenNodes; // key is an edge, Value is a Node
-
-    public Node(){}
-
-    public Node(T nodeLabel) {
-        //TODO
+/*
+	overview:
+	A Node is a mutable representation of a node with parent node 
+	and children nodes which conntected by edges with direction 
+		*/
+public class Node<L>{
+	//TODO add AF and REP variant
+	private L nodeLabel;
+	private Map<L, Node<L>> parentNodes; // key is an edge, Value is a Node
+	private Map<L, Node<L>> childrenNodes; // key is an edge, Value is a Node
+	
+    /**
+     * @effects Creates New node with label nodeLabel with no parents or children
+     */ 
+    public Node(L nodeLabel) {
         // the key in the map is an edge and the value is a node, either a parent or a child
         this.nodeLabel = nodeLabel;
         parentNodes = new HashMap<>();
         childrenNodes = new HashMap<>();
     }
 
-    public void addEdge(T edge, Node<T> childNode){
-        this.childrenNodes.put(edge, childNode);
+    public void addChildNode(L edge, Node<L> childNode){
+        if(!childrenNodes.containsValue(childNode)) {
+        	childrenNodes.putIfAbsent(edge, childNode);
+        }
+    }
 
-
+    public void addParentNode(L edge, Node<L> parentNode) {
+ 	   if(!parentNodes.containsValue(parentNode)) {
+ 		  parentNodes.putIfAbsent(edge, parentNode);
+        }
+    }
+         
+        
+    	
+        
     }
 
 
