b4347ce828f470e6d8ce543d2cd3f6a5ef1d1cee
baalon <baalon@LaptopBS>
2022-06-03 21:04:22 +0300

    Added Node.compareTo(node) and Node.getLabel()

--- oop2-ADT/src/homework2/Node.java
+++ oop2-ADT/src/homework2/Node.java
@@ -1,6 +1,7 @@
 package homework2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,9 +11,11 @@ import java.util.Map;
 	A Node is a mutable representation of a node with parent node 
 	and children nodes which conntected by edges with direction 
 		*/
-public class Node<L>{
+public class Node<L extends Comparable<L>> implements Comparable<Node<L>> {
 	//TODO add AF and REP variant
 	private L nodeLabel;
+//	private Map<L, Node<L>> parentNodes; // key is an edge, Value is a Node
+//	private Map<L, Node<L>> childrenNodes; // key is an edge, Value is a Node
 	private Map<L, Node<L>> parentNodes; // key is an edge, Value is a Node
 	private Map<L, Node<L>> childrenNodes; // key is an edge, Value is a Node
 	
@@ -26,29 +29,39 @@ public class Node<L>{
         childrenNodes = new HashMap<>();
     }
 
+    //TODO add specs 
     public void addChildNode(L edge, Node<L> childNode){
         if(!childrenNodes.containsValue(childNode)) {
         	childrenNodes.putIfAbsent(edge, childNode);
         }
     }
-
+  //TODO add specs 
     public void addParentNode(L edge, Node<L> parentNode) {
  	   if(!parentNodes.containsValue(parentNode)) {
  		  parentNodes.putIfAbsent(edge, parentNode);
         }
     }
-         
-    public ArrayList<T> getChildren() {
+  //TODO add specs 
+    public Object[] getChildren() {
 //        ArrayList<T> childrenList = new ArrayList<T>(childrenNodes.values());
         return childrenNodes.values().toArray().clone();
     }
-    public List<T> getParents() {
+  //TODO add specs 
+    public Object[] getParents() {
 //        ArrayList<T> parentsList = new ArrayList<T>(parentNodes.keySet());
 //        return Collections.unmodifiableList(parentsList);
     	return parentNodes.values().toArray().clone();
     }
-    	
-        
+    
+    public L getLabel() {
+    	return nodeLabel;
+    }
+    
+
+	@Override
+	public int compareTo(Node<L> node) {
+		return nodeLabel.compareTo(node.getLabel());
+	}
 }
 
 
