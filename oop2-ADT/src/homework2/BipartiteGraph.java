7279ee609dbea672d36d95d0ab3cf78450637c5c
baalon <baalon@LaptopBS>
2022-06-04 18:17:04 +0300

    Documentation and Specsfication of BipartiteGraph

--- oop2-ADT/src/homework2/BipartiteGraph.java
+++ oop2-ADT/src/homework2/BipartiteGraph.java
@@ -44,45 +44,43 @@ public class BipartiteGraph<T> {
 
     /**
      * @modifies this
-     * @effects if whiteNode!=null Add a node blackNode to this BipartiteGraph
-     * else do nothing
+     * @effects if whiteNode!=null and there is no other node with the same label
+ * 				Add a node blackNode to this BipartiteGraph
+ * 				else do nothing
      */
     public void addBlackNode(T blackNode) {
 //        Node<L> newBlackNode = new Node<>(blackNode);
 //        this.blackNodes.add(newBlackNode);
-    	if(blackNode!=null) {
-    	blackNodes.putIfAbsent(blackNode, new Node<>(blackNode));
+    	if(blackNode!=null || !this.contains(blackNode)) {
+    	blackNodes.put(blackNode, new Node<>(blackNode));
     	}
     }
 
 
     /**
      * @modifies this
-     * @effects if blackNode !=null Add a node blackNode to this BipartiteGraph
-     * else do nothing
+     * @effects if blackNode !=null and there is no other node with the same label
+     * 			Add a node blackNode to this BipartiteGraph
+     * 			else do nothing
      */
     public void addWhiteNode(T whiteNode) {
-    	if(whiteNode!=null) {
-    	whiteNodes.putIfAbsent(whiteNode, new Node<>(whiteNode));
+    	if(whiteNode!=null && !this.contains(whiteNode)) {
+    	whiteNodes.put(whiteNode, new Node<>(whiteNode));
     	}
     }
 
     /**
      * @modifies this
-     * @effects Adds an edge from the node sourceNode to the node destinationNode.
-     * 			The node is with label edgeLabel
-     *
+     * @effects Adds an edge with direction from the node sourceNode to the node destinationNode
+     * 			only if there is no other edge that connects between the nodes with the same direction
+     * 			or there is no edge with same name that goes from this node
+     * 			also destinationNode and sourceNode need to be of opposite type
+     * 			(destinationNode is white Node and sourceNode is black etc.)
+     * 			and destinationNode and sourceNode needs to be contained in the graph
+     * 			else do nothing
+     *			if the edge is created it will be labeled with the edgeLabel
      */
     public void addEdge(T sourceNode, T destinationNode, T edgeLabel) {
-        // check that nodes  are not null
-        // ?? check that the nodes are different??
-        // ?? check that the nodes of different type (i.e. black or white) ??
-        //if (!map.containsKey(sourceNode) || !map.containsKey(destinationNode) ) {
-        //    // nodes do not exist!!
-        //}
-        //map.get(sourceNode).add(destinationNode);
-//        this.blackNodes.get(0).addEdge(edgeLabel, (Node<T>) destinationNode);
-    	
     	//Check if src and dst are contained in the graph
     	if( !this.contains(destinationNode) || !this.contains(sourceNode) || edgeLabel == null) {
     		return;    		
@@ -100,43 +98,60 @@ public class BipartiteGraph<T> {
     	dst.addParentNode(edgeLabel, src.getLabel());
     	
     }
-
-    private Node<T> getNode(T node){
-    	Node<T> v = blackNodes.get(node);
-    	if(v!=null) return v;
-    	else return whiteNodes.get(node);
-    }
     
-    public boolean contains(T node) {
-    	return (blackNodes.containsKey(node) 
-    			|| whiteNodes.containsKey(node));
-    }
-    
-    //TODO add specs
+    /**
+     * @effects return a copy of Array of Objects that presents of all the Black Nodes in this
+     */
 	public Object[] listBlackNodes() {
 		return blackNodes.keySet().toArray();
 	}
-	//TODO add specs    
+    /**
+     * @effects return a copy of Array of Objects that presents of all the white Nodes in this
+     */
 	public Object[] listWhiteNodes() {
 		return whiteNodes.keySet().toArray();
 	}
-	//TODO add specs 
+    /**
+     * @effects return a copy of Array of Objects that presents children nodes of a parentName 
+     * 			or null if parentName is not in the graph
+     * 			if parentName has no children return empty array
+     */ 
 	public Object[] listChildren(T parentName) {
 		return getNode(parentName).getChildren();
 	}
-	//TODO add specs 
+    /**
+     * @effects return a copy of Array of Objects that presents parents nodes of a childName 
+     			or null if childName is not in the graph
+     * 			if childName has no parent return empty array
+     */  
 	public Object[] listParents(T childName) {
 		return getNode(childName).getParents();
 	}
-	//TODO add specs 
+    /**
+     * @effects return a label that represent child node that has parent node with label=parentName 
+     * 			and edge with label = edgeLabel
+     *			if parentName is not in the graph or there is no edgeLabel associated with parentName 
+     *			return null 
+     */  
    public T getChildByEdgeLabel(T parentName, T edgeLabel) {
 	   return getNode(parentName).getChild(edgeLabel);
    }
 	
+   /**
+    * @effects return a label that represent parent node that has child node with label=childName 
+    * 			and edge with label = edgeLabel
+    *			if childName is not in the graph or there is no edgeLabel associated with childName 
+    *			return null 
+    */ 
+   
    public T getParentByEdgeLabel(T childName, T edgeLabel) {
    return getNode(childName).getParent(edgeLabel);
 }
    
+   public boolean contains(T node) {
+	   return (blackNodes.containsKey(node) 
+			   || whiteNodes.containsKey(node));
+   }
     /**
      * Checks to see if the representation invariant is being violated
      * @throws AssertionError if representation invariant is violated
@@ -149,6 +164,13 @@ public class BipartiteGraph<T> {
 
     }
 
+    
+    private Node<T> getNode(T node){
+    	Node<T> v = blackNodes.get(node);
+    	if(v!=null) return v;
+    	else return whiteNodes.get(node);
+    }
+    
 
 
 
