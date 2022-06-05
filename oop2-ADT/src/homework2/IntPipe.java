6dc6777cf03c7a417c2d4df0c5fced963cdadd32
baalon <baalon@LaptopBS>
2022-06-05 11:49:18 +0300

    IntPipe

---
+++ oop2-ADT/src/homework2/IntPipe.java
@@ -0,0 +1,33 @@
+package homework2;
+
+import java.util.*;
+
+public class IntPipe implements Simulatable<String> {
+	//TODO add rep invariant
+	//TODO add Abstraction function
+	
+	private List<Integer> start;
+	private List<Integer> end;
+	
+	//TODO add specs
+	public IntPipe() {
+		start = new ArrayList<>();
+		end = new ArrayList<>();
+	}
+	//TODO add specs
+	public void add(Integer n) {
+		start.add(n);
+	}
+	//TODO add specs
+	public Integer getNext() {
+		return end.remove(0);
+	}
+	
+	//TODO add specs
+	@Override
+	public void simulate(BipartiteGraph<String> graph) {
+		end.addAll(start);
+		start.removeAll(start);
+	}
+	
+}
